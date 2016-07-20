70af5d86bb71388db7c59580a1c83a8a631c8fd8
rsuriya <rsuriya0396@gmail.com>
2016-07-20 15:37:54 +0530

    kk
    aa

---
+++ StringReverse.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.*;
+public class StringReverse {
+
+	public static void main(String[] args) {
+		String name="word";
+		
+		
+		StringBuffer b=new StringBuffer(name);
+		b.reverse();
+		System.out.println(b);
+		
+		
+		
+
+	}
+
+}
