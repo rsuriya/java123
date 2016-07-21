c7cfc1ad42854e8580a0a360810ece11d0b5e79e
rsuriya <rsuriya0396@gmail.com>
2016-07-21 16:23:32 +0530

    cc
    java234

---
+++ factorial.java
@@ -0,0 +1,21 @@
+
+public class Ass06 {
+	public int fact(int x)
+	{int result=1,i=1;
+		do
+			{result=result*i;
+			i++;
+			}
+			while(i<=x);
+		return result;
+			}
+	public static void main(String[] args) {
+	int a;
+	a=Integer.parseInt(args[0]);
+	Ass06 factorial=new Ass06();
+	int out=factorial.fact(a);
+	System.out.println("the factorial is="+out);
+
+	}
+
+}
