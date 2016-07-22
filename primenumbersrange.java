cd721fcdbe334bcf23d6e97319a6e05b906a8167
rsuriya <rsuriya0396@gmail.com>
2016-07-22 14:51:43 +0530

    prime
    range

---
+++ primenumbersrange.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int a;
+		int count=0;
+		int b;
+		Scanner in= new Scanner(System.in);
+		System.out.println("enter the numbers");
+		a=in.nextInt();
+		b=in.nextInt();
+		
+for(int i=a;i<=b;i++)
+	{	count=0;
+		for(int j=1;j<=i;j++)
+		{
+			if(i%j==0){count++;}
+			
+			}
+		if(count==2)
+		{System.out.println(i);}
+		else{continue;}
+		
+	}
+	
+}
+
